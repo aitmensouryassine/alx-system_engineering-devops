9250105fa4ea9121b4bcdad79d418873b9d0a3e8
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-10-23 11:27:58 +0000

    Gather data from API and Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Gather data from an API and export to CSV"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    BASE_URL = "https://jsonplaceholder.typicode.com"
+    user_id = sys.argv[1]
+
+    todos = requests.get(f"{BASE_URL}/todos?userId={user_id}").json()
+    user = requests.get(f"{BASE_URL}/users/{user_id}").json()
+
+    user_name = user.get('username')
+
+    csv_output = [[t.get('userId'), user_name, t.get('completed'),
+                   t.get('title')] for t in todos]
+
+    with open(f"{user_id}.csv", "w") as f:
+        write = csv.writer(f)
+        write.writerows(csv_output)
