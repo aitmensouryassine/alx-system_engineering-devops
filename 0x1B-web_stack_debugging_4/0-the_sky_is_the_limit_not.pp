1cf49c5f6c8f595608bf829aaee6c4c444cb406a
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-11-14 19:00:23 +0000

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,15 @@
-# Change the limit of open files by nginx
+# Define the path to the nginx configuration file
+$file_path = '/etc/default/nginx'
 
-exec { 'change_ulimit':
-  command => sed -i "s/ULIMIT.*/ULIMIT='-n 2000'/g" /etc/default/nginx,
-  path    => ['/bin', '/usr/bin']
+# Define the desired ULIMIT value
+$new_ulimit = 'ULIMIT="-n 2000"'
+
+exec { 'replace_ulimit':
+  command => "sed -i '/^ULIMIT=/c\\${new_ulimit}' ${file_path}",
+  path    => '/bin:/usr/bin',
+}
+
+# Run the command to restart Nginx
+exec { 'nginx-restart':
+  command     => '/usr/sbin/service nginx restart',
 }
